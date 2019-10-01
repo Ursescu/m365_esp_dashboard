47b30b311f765a562db466d7f35cd081cc6be57d
Ionut Ursescu <ionut.ursescu@nxp.com>
2019-10-01 11:01:38 +0300

    proto: Early implementation of process command function

--- main/proto.c
+++ main/proto.c
@@ -7,18 +7,14 @@
 /* Get current time macro */
 #define GET_TIME() ((uint32_t)(clock() * 1000 / CLOCKS_PER_SEC))
 
-typedef struct __stat {
-    uint8_t beep;
-} proto_stat;
+#define PROTO_CONSTANT (1.60934)
 
 /* Protocol status structure 
  * Maintaining the state between commands
  */
 proto_stat stats;
 
-
 uint32_t last_valid_message_time;
-
 
 uint8_t proto_verify_crc(uint8_t *message, uint8_t size) {
     unsigned long cksm = 0;
@@ -31,6 +27,7 @@ uint8_t proto_verify_crc(uint8_t *message, uint8_t size) {
     }
     return 0;
 }
+
 void proto_add_crc(uint8_t *message, uint8_t size) {
     unsigned long cksm = 0;
     for (int i = 2; i < size - 2; i++)
@@ -54,11 +51,9 @@ uint16_t proto_crc(const uint8_t *data, uint16_t size) {
     return ret;
 }
 
-
-{0x55, 0xAA, 0x4, 0x20, 0x3, 0x7C, 0x0, 0x0, 0x5C, 0xFF};
+// {0x55, 0xAA, 0x4, 0x20, 0x3, 0x7C, 0x0, 0x0, 0x5C, 0xFF};
 
 static uint8_t connected() {
-
     /* Some way to measure the timeout, in case connection dropped */
 
     if (GET_TIME() - last_valid_message_time >= COMM_TIMEOUT) {
@@ -71,12 +66,54 @@ static uint8_t connected() {
 
 static void process_command(comm_chan *channel, QueueHandle_t display_queue) {
     /* Verify crc */
+    if (!proto_verify_crc(channel->rx, channel->rx_size))
+        return;
 
-    /* Insert display queue if necessary */
+    /* Update last good message */
+    last_valid_message_time = GET_TIME();
 
-    /* If crc status ok */
-    last_valid_message_time = GET_TIME(); 
+    const char *message = channel->rx;
 
+    switch (message[1]) {
+        case 0x21:
+            switch (message[2]) {
+                case 0x01:
+                    if (message[2] == 0x61)
+                        stats.tail = message[3];
+                    break;
+                case 0x64:
+                    stats.eco = message[4];
+                    stats.led = message[5];
+                    stats.night = message[6];
+                    stats.beep = message[7];
+                    break;
+            }
+            break;
+        case 0x23:
+            switch (message[3]) {
+                case 0x7B:
+                    stats.ecoMode = message[4];
+                    stats.cruise = message[6];
+                    break;
+                case 0x7D:
+                    stats.tail = message[4];
+                    break;
+                case 0xB0:
+                    stats.alarmStatus = message[6];
+                    stats.lock = message[8];
+                    stats.battery = message[12];
+                    stats.velocity = (message[14] + (message[15] * 256)) / 1000 / PROTO_CONSTANT;
+                    stats.averageVelocity = (message[16] + (message[17] * 265)) / 1000 / PROTO_CONSTANT;
+                    stats.odometer = (message[18] + (message[19] * 256) + (message[20] * 256 * 256)) / 1000 / PROTO_CONSTANT;
+                    stats.temperature = ((message[26] + (message[27] * 256)) / 10 * 9 / 5) + 32;
+                    if (stats.alarmStatus)
+                        tone(input.buzzer, 20, 400);
+                    break;
+            }
+    }
+
+    /* Send some data to display */
+    /* To be implemented */
 }
 
 void proto_command(comm_chan *channel, QueueHandle_t display_queue) {
@@ -99,7 +136,6 @@ void proto_command(comm_chan *channel, QueueHandle_t display_queue) {
     }
 
     /* Iterate over all message types and collect informations */
-
     switch (messageType++) {
         case 0:
             /* Just for sake of completeness */
