b3c2b757f2343c24eb3ab095442335881dc84c1a
Ionut Ursescu <ionut.ursescu@nxp.com>
2019-10-01 10:59:39 +0300

    proto: Added stat structure that objectify the current state.

--- main/proto.h
+++ main/proto.h
@@ -12,6 +12,27 @@
 #define CRC1_MASK (0x00ff)
 #define CRC1_SHIFT (0)
 
+typedef struct __stat {
+    uint8_t alarmStatus;
+    uint8_t averageVelocity;
+    uint8_t battery;
+    uint8_t beep;
+    uint8_t brakeConnected;
+    uint8_t cruise;
+    uint8_t eco;
+    uint8_t ecoMode;
+    uint8_t hasConnected;
+    uint8_t isConnected;
+    uint8_t led;
+    uint8_t lock;
+    uint8_t night;
+    uint8_t odometer;
+    uint8_t tail;
+    uint8_t temperature;
+    uint8_t throttleConnected;
+    uint8_t velocity;
+} proto_stat;
+
 void proto_add_crc(uint8_t *, uint8_t);
 uint8_t proto_verify_crc(uint8_t *, uint8_t);
 uint16_t proto_crc(const uint8_t *, uint16_t);
