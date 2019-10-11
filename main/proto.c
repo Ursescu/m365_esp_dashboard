2c4b903a9d9dc93430a0a8f832647818596e43ca
ursescuionut <ursescuionut97@gmail.com>
2019-10-11 17:18:16 +0300

    proto: Fix process command offsets

--- main/proto.c
+++ main/proto.c
@@ -70,52 +70,72 @@ static void process_command(const uint8_t *command, uint16_t size) {
     /* Verify crc */
     if (!proto_verify_crc(command, size))
         return;
-    // printf("After CRC \n");
-    // print_command(command, size);
+
 
     /* Update last good message */
     last_valid_message_time = GET_TIME();
 
-    // switch (command[1]) {
-    //     case 0x21:
-    //         switch (command[2]) {
-    //             case 0x01:
-    //                 if (command[2] == 0x61)
-    //                     stats.tail = message[3];
-    //                 break;
-    //             case 0x64:
-    //                 stats.eco = command[4];
-    //                 stats.led = command[5];
-    //                 stats.night = command[6];
-    //                 stats.beep = command[7];
-    //                 break;
-    //         }
-    //         break;
-    //     case 0x23:
-    //         switch (command[3]) {
-    //             case 0x7B:
-    //                 stats.ecoMode = command[4];
-    //                 stats.cruise = command[6];
-    //                 break;
-    //             case 0x7D:
-    //                 stats.tail = command[4];
-    //                 break;
-    //             case 0xB0:
-    //                 stats.alarmStatus = command[6];
-    //                 stats.lock = command[8];
-    //                 stats.battery = command[12];
-    //                 stats.velocity = (command[14] + (command[15] * 256)) / 1000 / PROTO_CONSTANT;
-    //                 stats.averageVelocity = (command[16] + (command[17] * 265)) / 1000 / PROTO_CONSTANT;
-    //                 stats.odometer = (command[18] + (command[19] * 256) + (command[20] * 256 * 256)) / 1000 / PROTO_CONSTANT;
-    //                 stats.temperature = ((command[26] + (command[27] * 256)) / 10 * 9 / 5) + 32;
-    //                 break;
-    //         }
-    // }
+    switch (command[3]) {
+        case 0x21:
+            switch (command[4]) {
+                case 0x01:
+                    if (command[4] == 0x61)
+                        stats.tail = command[5];
+                    break;
+                case 0x64:
+                    stats.eco = command[6];
+                    stats.led = command[7];
+                    stats.night = command[8];
+                    stats.beep = command[9];
+                    break;
+            }
+            break;
+        case 0x23:
+            switch (command[5]) {
+                case 0x7B:
+                    stats.ecoMode = command[6];
+                    stats.cruise = command[8];
+                    break;
+                case 0x7D:
+                    stats.tail = command[6];
+                    break;
+                case 0xB0:
+                    stats.alarmStatus = command[8];
+                    stats.lock = command[10];
+                    stats.battery = command[14];
+                    stats.velocity = (command[16] + (command[17] * 256)) / 1000 / PROTO_CONSTANT;
+                    stats.averageVelocity = (command[18] + (command[19] * 265)) / 1000 / PROTO_CONSTANT;
+                    stats.odometer = (command[20] + (command[21] * 256) + (command[22] * 256 * 256)) / 1000 / PROTO_CONSTANT;
+                    stats.temperature = ((command[28] + (command[29] * 256)) / 10 * 9 / 5) + 32;
+                    break;
+            }
+    }
+
+    printf("State \n");
+    printf("tail: %hhu \n", stats.tail);
+    printf("eco: %hhu \n", stats.eco);
+    printf("led: %hhu \n", stats.led);
+    printf("night: %hhu \n", stats.night);
+    printf("beep: %hhu \n", stats.beep);
+    printf("eco: %hhu \n", stats.ecoMode);
+    printf("cruise: %hhu \n", stats.cruise);
+    printf("lock: %hhu \n", stats.lock);
+    printf("battery: %hhu \n", stats.battery);
+    printf("velocity: %hhu \n", stats.velocity);
+    printf("averageVelocity: %hhu \n", stats.averageVelocity);
+    printf("odometer: %hhu \n", stats.odometer);
+    printf("temperature: %hhu \n", stats.temperature);
+
+    if(stats.alarmStatus) {
+        printf("Beeeep \n");
+        stats.alarmStatus = 0;
+    }
+
 }
 
 static void process_buffer(comm_chan *channel, QueueHandle_t display_queue) {
     uint16_t buffer_size = channel->rx_size - channel->tx_size;
-    // printf("buffer_size %hu %hu %hu \n", channel->rx_size, channel->tx_size, buffer_size);
+    printf("buffer_size %hu %hu %hu \n", channel->rx_size, channel->tx_size, buffer_size);
     /* Hack to eliminate what it's send on tx */
     if (buffer_size <= 0 || buffer_size > COMM_BUFF_SIZE) {
         /* Nothing to receive */
@@ -181,6 +201,7 @@ void proto_command(comm_chan *channel, QueueHandle_t display_queue) {
             // printf("SEND DATA Command %d \n", messageType - 1);
             proto_add_crc(command, sizeof(command));
             comm_copy_tx_chan(channel, command, sizeof(command));
+            if(stats.beep) stats.beep = 0;
             break;
         }
         case 4: {
@@ -189,6 +210,7 @@ void proto_command(comm_chan *channel, QueueHandle_t display_queue) {
             // printf("CONNECT Command %d \n", messageType - 1);
             // printf("Command size %d \n", sizeof(command));
             comm_copy_tx_chan(channel, command, sizeof(command));
+            if(stats.beep) stats.beep = 0;
             break;
         }
         case 5: {
